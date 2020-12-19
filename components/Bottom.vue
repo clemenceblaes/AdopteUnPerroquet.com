570a0d65126ffa385e1f0c675c54bdf398db7fc1
BLAES Clémence <clemence.blaes@laposte.net>
2020-12-19 11:21:07 +0100

    Add Bottom.vue
    This is the component used for the footer.

---
+++ components/Bottom.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="bottom">
+    <div class="logo col-4"></div>
+    <div class="contact col-4">
+      ADOPTEUNPERROQUET.COM<br />
+      Le site de rencontre entre éleveurs, associations et particuliers.<br />
+      Contact : hello@adopteunperroquet.com
+    </div>
+    <div class="menu col-4">
+      <li><router-link to="/">Acceuil</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+      <li><router-link to="/auth">Mon compte</router-link></li>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Bottom",
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;500&display=swap");
+
+a {
+  font-size: 18px;
+  font-family: "Roboto", sans-serif;
+  font-weight: bold;
+  color: #709255;
+}
+
+.bottom {
+  width: 100%;
+  display: flex;
+}
+
+.logo {
+  background: url("https://zupimages.net/up/20/51/zath.png");
+  background-size: cover;
+  height: 200px;
+}
+
+.contact,
+.menu {
+  margin: auto;
+}
+
+.menu {
+  text-transform: uppercase;
+  list-style: none;
+}
+</style>
