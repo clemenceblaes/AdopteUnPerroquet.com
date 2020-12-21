bbcea311ce262db68c311be52a035c78cedba8c4
BLAES Clémence <clemence.blaes@laposte.net>
2020-12-22 00:11:36 +0100

    Add files via upload

---
+++ components/Top.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app-bar app color="#1f1d1d" dark height="125">
+    <div class="d-flex align-center">
+      <v-img
+        alt="AdopteUnPerroquet Logo"
+        class="shrink mr-2"
+        contain
+        src="https://zupimages.net/up/20/51/zath.png"
+        transition="scale-transition"
+        width="400"
+        height="300"
+      />
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn href="" target="_blank" text>
+      <span class="mr-2">Se connecter</span>
+      <v-icon>mdi-account</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
