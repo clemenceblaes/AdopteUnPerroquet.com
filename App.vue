de5730d114650df38e3135f39fa4505e05ffb3a1
BLAES Clémence <clemence.blaes@laposte.net>
2020-12-22 00:13:11 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <Top />
+    <v-main>
+      <router-view />
+    </v-main>
+
+    <Bottom />
+  </v-app>
+</template>
+
+<script>
+import Top from "./components/Top.vue";
+import Bottom from "./components/Bottom.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    Bottom,
+    Top,
+  },
+};
+</script>
