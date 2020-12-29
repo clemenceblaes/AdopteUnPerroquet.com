8b21fb5c449c84eca98cf57e833b0c2f23121083
BLAES Clémence <clemence.blaes@laposte.net>
2020-12-29 13:48:35 +0100

    Add Home.vue

---
+++ views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <Menu />
+    </v-row>
+    <v-row id="carousel">
+      <Carousel />
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Menu from "@/components/Menu.vue";
+import Carousel from "@/components/Carousel.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Menu,
+    Carousel,
+  },
+};
+</script>
+
+<style scoped>
+#carousel {
+  margin-top: 1.3%;
+}
+</style>
+
+
