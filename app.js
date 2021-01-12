b92c8a561d1d530428919809ae5633c897789d0d
BLAES Clémence <clemence.blaes@laposte.net>
2021-01-12 18:03:26 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,62 @@
+window.Vue = require('vue');
+
+import vuetify from './vuetify';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import Store from "./store";
+import MainCarousel from "./components/Carousels/MainCarousel.vue";
+import ParrotMenu from "./components/Views/ParrotMenuView.vue";
+import littleParrots from "./components/Views/LittleParrotsView.vue";
+import mediumParrots from "./components/Views/MediumParrotsView.vue";
+import largeParrots from "./components/Views/largeParrotsView.vue";
+import Guide from "./components/Guide.vue";
+import Auth from "./components/Auth.vue";
+import NewAccount from "./components/newAccount.vue";
+
+
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: "/",
+        component: MainCarousel,
+    },
+    {
+        path: "/parrots",
+        component: ParrotMenu,
+    },
+    {
+        path: "/littleParrots",
+        component: littleParrots,
+    },
+    {
+        path: "/mediumParrots",
+        component: mediumParrots,
+    },
+    {
+        path: "/largeParrots",
+        component: largeParrots,
+    },
+    {
+        path: "/guide",
+        component: Guide,
+    },
+    {
+        path: "/auth",
+        component: Auth,
+    },
+    {
+        path: "/newAccount",
+        component: NewAccount,
+    }
+];
+
+const router = new VueRouter({ routes });
+
+const app = new Vue({
+    vuetify,
+    router: router,
+    Store,
+    el: '#app',
+    render: h => h(App),
+});
