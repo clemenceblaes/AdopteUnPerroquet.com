5b34242373b2956cbc638685fb160dd9de50738c
BLAES Clémence <clemence.blaes@laposte.net>
2021-01-27 21:06:07 +0100

    Update App.vue

--- App.vue
+++ App.vue
@@ -1,24 +1,51 @@
 <template>
   <v-app>
-    <Top />
-    <v-main>
-      <Menu />
-      <router-view />
-    </v-main>
-    <Bottom />
+    <navbar :app="this" class="mb-2"></navbar>
+
+    <spinner v-if="loading"></spinner>
+    <div v-else-if="initiated"><router-view :app="this" /></div>
+    <v-footer class="mt-6" color="#709255" padless
+      ><bottombar id="bottombar"
+    /></v-footer>
   </v-app>
 </template>
 
 <script>
-import Top from "./components/Top.vue";
-import Menu from "./components/Menu.vue";
-import Bottom from "./components/Bottom.vue";
+import Navbar from "./components/Navbar.vue";
+import Bottombar from "./components/Bottombar.vue";
 
 export default {
+  name: "app",
   components: {
-    Bottom,
-    Menu,
-    Top,
+    Navbar,
+    Bottombar,
+  },
+  data() {
+    return {
+      user: null,
+      loading: false,
+      initiated: false,
+      req: axios.create({
+        baseUrl: BASE_URL,
+      }),
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      this.loading = true;
+
+      this.req.get("auth/init").then((response) => {
+        this.user = response.data.user;
+        this.loading = false;
+        this.initiated = true;
+      });
+    },
   },
 };
 </script>
+
+<style scoped>
+</style>
