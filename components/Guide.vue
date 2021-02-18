33ed880db5d5d8788e42de4ed809424162b5d0d1
BLAES Clémence <clemence.blaes@laposte.net>
2021-02-18 14:50:29 +0100

    Update Guide.vue

--- components/Guide.vue
+++ components/Guide.vue
@@ -1,19 +1,19 @@
 <template>
   <v-container col-8>
-    <h1>Guide de l'espèce</h1>
-    <h2>Généralités</h2>
+    <h1 class="mt-4 mb-8 text-center font-weight-bold">Guide de l'espèce</h1>
+    <h2 class="mt-8 mb-4 text-center font-weight-bold">Généralités</h2>
     <p v-if="getParrotGuide">
       {{ getParrotGuide.desc1 }}
     </p>
-    <h2>Caractère</h2>
+    <h2 class="mt-8 mb-4 text-center font-weight-bold">Caractère</h2>
     <p v-if="getParrotGuide">
       {{ getParrotGuide.desc2 }}
     </p>
-    <h2>Alimentation</h2>
+    <h2 class="mt-8 mb-4 text-center font-weight-bold">Alimentation</h2>
     <p v-if="getParrotGuide">
       {{ getParrotGuide.desc3 }}
     </p>
-    <h2>Mode de vie</h2>
+    <h2 class="mt-8 mb-4 text-center font-weight-bold">Mode de vie</h2>
     <p v-if="getParrotGuide">
       {{ getParrotGuide.desc4 }}
     </p>
@@ -22,7 +22,7 @@
         {{ desc }}
       </li>
     </div>
-    <h2>Les soins</h2>
+    <h2 class="mt-8 mb-4 text-center font-weight-bold">Les soins</h2>
     <p v-if="getParrotGuide">
       {{ getParrotGuide.desc5 }}
     </p>
@@ -31,13 +31,13 @@
         {{ desc }}
       </li>
     </div>
-    <h2>La reproduction</h2>
+    <h2 class="mt-8 mb-4 text-center font-weight-bold">La reproduction</h2>
     <p v-if="getParrotGuide">
       {{ getParrotGuide.desc6 }}
     </p>
 
-    <p class="mt-4 text-center">
-      <v-btn class="return" color="#709255" text
+    <p class="mt-16 text-center">
+      <v-btn class="return" text
         ><router-link to="/parrots"> Retour </router-link></v-btn
       >
     </p>
@@ -45,54 +45,39 @@
 </template>
 
 <script>
-import Store from "../store/modules/CurrentUsers.js";
 export default {
   data() {
     return {};
   },
   computed: {
     getParrotGuide: function () {
-      return Store.state.guide;
+      return this.$store.state.guide;
     },
-  },
-  beforeDestroy() {
-    Store.mutations.RESET_GUIDE(Store.state.guide);
   },
 };
 </script>
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Baloo+Thambi+2:wght@500;700&display=swap");
-
 h1 {
-  margin: 3% auto 2% auto;
   color: #709255;
   font-family: "Baloo Thambi 2", sans-serif;
   font-size: 3rem;
-  font-weight: 700;
-  text-align: center;
 }
-
 h2 {
-  margin: 3% auto 2% auto;
   color: #709255;
   font-family: "Baloo Thambi 2", sans-serif;
   font-size: 2.5rem;
-  font-weight: 700;
-  text-align: center;
 }
-
 p,
 li {
   font-family: "Baloo Thambi 2", sans-serif;
 }
-
 a {
   text-decoration: none;
   color: white !important;
   font-family: "Baloo Thambi 2", sans-serif;
 }
-
 .return {
   background-color: #709255;
 }
