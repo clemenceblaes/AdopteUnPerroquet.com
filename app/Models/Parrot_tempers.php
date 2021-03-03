421e84c084f6b66e72ac3e49cf87883daa49c1d9
BLAES Clémence <clemence.blaes@laposte.net>
2021-03-03 14:04:40 +0100

    Create Parrot_tempers.php

---
+++ app/Models/Parrot_tempers.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Parrot_tempers extends Model
+{
+    use HasFactory;
+}
